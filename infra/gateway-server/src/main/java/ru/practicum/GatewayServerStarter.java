fe96ee2f5dcfdc82ede8f8c4386f410d91585652
Y7tion <farnitin@yandex.ru>
2026-01-18 14:24:02 +0400

    add EnableDiscoveryClient annotation for gateway-server

--- infra/gateway-server/src/main/java/ru/practicum/GatewayServerStarter.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayServerStarter.java
@@ -2,9 +2,9 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServerStarter {
     public static void main(String[] args) {
