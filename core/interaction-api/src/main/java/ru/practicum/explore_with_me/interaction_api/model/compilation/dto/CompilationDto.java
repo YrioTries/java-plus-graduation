b97fe024a617f52b6d51bfa17c138a12b2be8cec
Y7tion <farnitin@yandex.ru>
2026-01-26 08:58:52 +0400

    change set to list

--- core/interaction-api/src/main/java/ru/practicum/explore_with_me/interaction_api/model/compilation/dto/CompilationDto.java
+++ core/interaction-api/src/main/java/ru/practicum/explore_with_me/interaction_api/model/compilation/dto/CompilationDto.java
@@ -5,13 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.explore_with_me.interaction_api.model.event.dto.EventShortDto;
 
-import java.util.Set;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompilationDto {
-    private Set<EventShortDto> events;
+    private List<EventShortDto> events;
     private Long id;
     private Boolean pinned;
     private String title;
