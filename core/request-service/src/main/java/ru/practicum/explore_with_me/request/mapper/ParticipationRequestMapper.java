d85dff8c1af4f0a7166010bd2cc30f33a50ebb95
Y7tion <farnitin@yandex.ru>
2026-01-26 08:43:49 +0400

    change mapping in request-service

--- core/request-service/src/main/java/ru/practicum/explore_with_me/request/mapper/ParticipationRequestMapper.java
+++ core/request-service/src/main/java/ru/practicum/explore_with_me/request/mapper/ParticipationRequestMapper.java
@@ -1,14 +1,10 @@
 package ru.practicum.explore_with_me.request.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.practicum.explore_with_me.interaction_api.model.request.dto.ParticipationRequestDto;
 import ru.practicum.explore_with_me.request.dao.ParticipationRequest;
 
 @Mapper(componentModel = "spring")
 public interface ParticipationRequestMapper {
-
-    @Mapping(target = "eventId", source = "eventId")
-    @Mapping(target = "requesterId", source = "requesterId")
     ParticipationRequestDto toParticipationRequestDto(ParticipationRequest participationRequest);
 }
