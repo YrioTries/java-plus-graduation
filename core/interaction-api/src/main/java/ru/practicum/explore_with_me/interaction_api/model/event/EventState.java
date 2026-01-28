f52aa5be692e36df79664097e6147ead3ba15286
Y7tion <farnitin@yandex.ru>
2026-01-28 19:10:41 +0400

    add toString to EventState

--- core/interaction-api/src/main/java/ru/practicum/explore_with_me/interaction_api/model/event/EventState.java
+++ core/interaction-api/src/main/java/ru/practicum/explore_with_me/interaction_api/model/event/EventState.java
@@ -1,5 +1,10 @@
 package ru.practicum.explore_with_me.interaction_api.model.event;
 
 public enum EventState {
-    PENDING, PUBLISHED, CANCELED
+    PENDING, PUBLISHED, CANCELED;
+
+    @Override
+    public String toString() {
+        return name();
+    }
 }
