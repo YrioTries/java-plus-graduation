b3949892b2e2ccf8a4f7d0fa312aed714343d445
Y7tion <farnitin@yandex.ru>
2026-02-09 01:24:09 +0400

    change collector settings

--- stats/telemetry/collector/src/main/java/ru/practicum/collector/config/KafkaConfig.java
+++ stats/telemetry/collector/src/main/java/ru/practicum/collector/config/KafkaConfig.java
@@ -1,7 +1,6 @@
 package ru.practicum.collector.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
