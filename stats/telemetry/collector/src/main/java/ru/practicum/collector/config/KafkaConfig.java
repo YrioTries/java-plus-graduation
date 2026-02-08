34451f993da7f98e6799ae1888338b40bfc34357
Y7tion <farnitin@yandex.ru>
2026-02-09 02:56:01 +0400

    debug config

--- stats/telemetry/collector/src/main/java/ru/practicum/collector/config/KafkaConfig.java
+++ stats/telemetry/collector/src/main/java/ru/practicum/collector/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package ru.practicum.collector.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,4 +13,13 @@ import java.util.Properties;
 public class KafkaConfig {
     private Properties properties = new Properties();
     private String topic;
+
+    @PostConstruct
+    public void debug() {
+        System.out.println("=== KAFKA CONFIG DEBUG ===");
+        System.out.println("properties: " + properties);
+        System.out.println("keys: " + properties.stringPropertyNames());
+        System.out.println("key.serializer: " + properties.getProperty("key.serializer"));
+        System.out.println("========================");
+    }
 }
